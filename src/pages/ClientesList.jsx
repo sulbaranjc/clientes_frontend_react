4fb4597bcd2388e3356e7f0ce66bdf32d616b11d
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2026-02-06 16:11:32 +0100

    Preparación al pase de producción

--- src/pages/ClientesList.jsx
+++ src/pages/ClientesList.jsx
@@ -79,7 +79,7 @@ async function confirmarEliminacion() {
     <Container>
       <h1 className="text-center mb-4">Gestión de Clientes</h1>
       <p className="lead text-center mb-5">
-        Lista de clientes registrados en el sistema. deploy by Juan..
+        Lista de clientes registrados en el sistema. deploy by Juan Carlos..
       </p>
 
       <div className="mb-3">
